88ac5529e0b075eba0e7c710f2bae34c6f3378ad
Dmitry Osipov <deosipov@mail.ru>
2022-07-26 23:05:26 +0300

    Задача 8.1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int port = 8081;
+        System.out.println("Запустили сервер");
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // ждем подключения
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                System.out.printf("Принято новое соединенение. Порт: %d%n", clientSocket.getPort());
+                String name = in.readLine();
+                out.println(String.format("Привет %s, твой порт: %d", name, clientSocket.getPort()));
+            }
+        }
+    }
+}
